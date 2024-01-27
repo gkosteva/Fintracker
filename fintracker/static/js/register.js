2377dd920e804121bbd8b94788e1cb16c62c6927
Gabriela Kosteva <gabriela.kosteva@paysafe.com>
2024-01-27 14:36:13 +0200

    Adding email validation

--- fintracker/static/js/register.js
+++ fintracker/static/js/register.js
@@ -1,8 +1,9 @@
 const usernameField = document.querySelector("#usernameField")
-const feedbackField = document.querySelector(".invalid-feedback")
+const feedbackField = document.querySelector(".invalid_feedback")
+const emailField = document.querySelector("#emailField")
+const emailFeedbackField = document.querySelector(".emailFeedback")
 
 usernameField.addEventListener("keyup", (e) => {
-    console.log("777777", 777777)
     const usernameVal = e.target.value;
 
     if (usernameVal.length > 0) {
@@ -21,8 +22,36 @@ usernameField.addEventListener("keyup", (e) => {
                 usernameField.classList.add("is-valid");
             }
         });
-    }
-    else{
+    } else {
         usernameField.classList.remove("is-valid");
+        usernameField.classList.remove("is-invalid");
+        feedbackField.style.display = 'none';
+    }
+});
+
+emailField.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+
+
+    if (emailVal.length > 0) {
+        fetch("/authentication/validate-email",
+            {
+                body: JSON.stringify({email: emailVal}), method: 'POST'
+            }).then((res) => res.json()).then((data) => {
+            console.log('data', data)
+            if (data.email_error) {
+                emailField.classList.add("is-invalid");
+                emailFeedbackField.style.display = 'block';
+                emailFeedbackField.innerHTML = `<p>${data.email_error}</p>`;
+            } else {
+                emailFeedbackField.style.display = 'none'
+                emailField.classList.remove("is-invalid");
+                emailField.classList.add("is-valid");
+            }
+        });
+    } else {
+        emailField.classList.remove("is-valid");
+        emailField.classList.remove("is-invalid");
+        emailFeedbackField.style.display = 'none';
     }
 });
